77b9c2ec6d2c05ab52bb46ca690574c405992bbc
Felipe Andery Reis <fandery@inatel.br>
2020-12-12 09:33:52 -0300

    Atualizando pagina Home

--- iot_project/src/app/pages/home/home.page.ts
+++ iot_project/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
+
+  goToTempPage(){
+    this.router.navigate(['temperature']);
+  }
+
+
 
 }
